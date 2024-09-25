e67a172462a8ea8c9eab5c394d254b3ac29e2803
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-09-25 17:26:17 +0900

    ✨ user一覧機能も追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 module Api
   module V1
     class Api::V1::UsersController < ApplicationController
+      def index
+        user = User.all
+        render json: user
+      end
+
       def create
         # 条件に該当するデータがあればそれを返す。なければ新規作成
         user = User.find_or_create_by(provider: params[:provider], uid: params[:uid], name: params[:name], email: params[:email])
